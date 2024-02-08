f4915c44200cb2a225d494ab4af5b9d81fdc3dd7
AndhikaRahmatullah <dikarahmatullah909@gmail.com>
2024-02-08 18:35:14 +0700

    resize font

--- src/components/loading/loading-screen.tsx
+++ src/components/loading/loading-screen.tsx
@@ -5,13 +5,8 @@ import './loading-screen.css';
 
 const LoadingScreen: React.FC = () => {
   return (
-<<<<<<< HEAD
     <div className="mx-5 flex h-screen w-full items-center justify-center">
       <div className="loader text-center text-4xl md:text-7xl" />
-=======
-    <div className="flex h-screen items-center justify-center">
-      <div className="loader" />
->>>>>>> d146d3d272ae3093fa46745f66d13725ccc98bca
     </div>
   );
 };
