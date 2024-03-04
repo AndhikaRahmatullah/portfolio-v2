c58b1191c668e2686d18bb93271f656ff4dd9c88
AndhikaRahmatullah <dikarahmatullah909@gmail.com>
2024-03-04 16:24:29 +0700

    fix: meta

--- src/pages/home/app.tsx
+++ src/pages/home/app.tsx
@@ -11,10 +11,13 @@ const HomePage: React.FC = () => {
   return (
     <>
       <Helmet>
-        <meta charSet="utf-8" />
         <title>Andhika Rahmatullah</title>
-        <link rel="canonical" href="https://andhikar-portfolio-v2.vercel.app" />
-        <meta name="keywords" content="andhika,portfolio,andhika rahmatullah,frontend" />
+        <link rel="canonical" href="/" />
+        <meta
+          name="description"
+          content="I am an experienced frontend programmer with extensive skills in developing various types of projects. Over the years, I have been involved in numerous freelance projects including the
+    creation of backoffice systems, e-commerce platforms, and company profile websites."
+        />
       </Helmet>
 
       <MainLayout>
